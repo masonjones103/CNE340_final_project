a8ba1bf1e537e7da4c1a918848d7a56220d4540a
Kaylie Velazquez-Becerra <kjvelazquezbecerra@student.rtc.edu>
2025-03-12 14:51:12 -0700

    Update import.py
    Added the following:
    
    imports, merging driver and unnamed: 2 columns, dropping the last row/column that contain the unwanted link.

--- import.py
+++ import.py
@@ -1,13 +1,11 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from sqlalchemy import create_engine
 
+
 # urls for data
-urls =  {
-        "stats_drivers": "https://docs.google.com/spreadsheets/d/1QZ3EeNsdmd1R6Ahow5xr6Z_CcnvdHYm2uVXsivKFOa8/export?format=csv&gid=0",
-        "stats_bodies": "https://docs.google.com/spreadsheets/d/1QZ3EeNsdmd1R6Ahow5xr6Z_CcnvdHYm2uVXsivKFOa8/export?format=csv&gid=1891506518",
-        "stats_wheels": "https://docs.google.com/spreadsheets/d/1QZ3EeNsdmd1R6Ahow5xr6Z_CcnvdHYm2uVXsivKFOa8/export?format=csv&gid=54515627",
-        "stats_gliders": "https://docs.google.com/spreadsheets/d/1QZ3EeNsdmd1R6Ahow5xr6Z_CcnvdHYm2uVXsivKFOa8/export?format=csv&gid=206723924",
-}
+urls =  {"stats_drivers": "https://docs.google.com/spreadsheets/d/1QZ3EeNsdmd1R6Ahow5xr6Z_CcnvdHYm2uVXsivKFOa8/export?format=csv&gid=0"}
 
 # creates engine for sql alchemy and mysql stuff
 engine = create_engine('mysql+mysqlconnector://root:@localhost/mario_kart')
@@ -18,6 +16,14 @@ for sheet_name, url in urls.items():
 
 # reads csv data from url
         df = pd.read_csv(url, header=1)
+
+# merging Driver and Unnamed: 2 columns
+        if "Driver" in df.columns and "Unnamed: 2" in df.columns:
+                df["Driver"] = df["Driver"].fillna(df["Unnamed: 2"])
+                df.drop(columns=["Unnamed: 2"], inplace=True)
+
+# Dropping last row/column
+        df = df.drop(52, axis=0)
 
 # saves dataframe to mysql
         df.to_sql(name=sheet_name, con=engine, if_exists="replace", index=False, method="multi")
