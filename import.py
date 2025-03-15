24357152a1baa5a579b3d6c2aefed7dd0d7ab222
Kaylie Velazquez-Becerra <kjvelazquezbecerra@student.rtc.edu>
2025-03-14 21:15:09 -0700

    Update import.py
    Drivers’ vs AC & OF
    
    Seeing which drivers have an advantage in AC & OF & which are disadvantaged. Some drivers have advantage in AC but not in OF or the other way around.

--- import.py
+++ import.py
@@ -29,3 +29,43 @@ for sheet_name, url in urls.items():
         df.to_sql(name=sheet_name, con=engine, if_exists="replace", index=False, method="multi")
 
 print("data has been imported")
+# ---------------------------------------------Kaylie's Coding----------------------------------------------------------
+input('''
+Kaylie's analysis:
+Press enter:
+''')
+
+def analysis_1():
+    temp_df = df.loc[:10]
+
+# Drivers vs. AC & WG
+df = df.loc[:10]
+
+if "AC" in df.columns and "OF" in df.columns:
+    df["AC"] = pd.to_numeric(df["AC"], errors='coerce')
+    df["OF"] = pd.to_numeric(df["OF"], errors='coerce')
+
+    AC_M = df["AC"].mean()
+    OF_M = df["OF"].mean()
+
+    for drivers, row in df.iterrows():
+        ac_dis_adv = "AC Advantage" if row["AC"] > AC_M else "AC Disadvantage"
+        of_dis_adv = "OF Advantage" if row["OF"] > OF_M else "OF Disadvantage"
+        print(f"{row['Driver']} has {ac_dis_adv} & {of_dis_adv}")
+
+    bar_width = 0.40
+    x = np.arange(len(df))
+    plt.figure(figsize=(15, 6))
+    plt.bar(x - bar_width / 2, df["AC"], label="AC", alpha=0.8, color='pink', width=bar_width)
+    plt.bar(x + bar_width / 2, df["OF"], label="OF", alpha=0.8, color="purple", width=bar_width)
+
+    plt.title("Driver vs AC & OF")
+    plt.xlabel("Driver")
+    plt.ylabel("AC & OF Stats")
+    plt.xticks(x, df["Driver"], rotation=45)
+    plt.grid(True)
+    plt.legend()
+    plt.show()  # Display bar graph
+    plt.clf()  # Clears figure
+    plt.close()  # Closes figure
+
